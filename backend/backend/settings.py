983b57a2fc68d8c6530b805b2836a86585b536bc
Larion Bashta <larionhich@gmail.com>
2025-01-23 16:15:07 +0300

    21

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.165.254', '127.0.0.1', 'localhost', 'gioul.gotdns.ch']
+ALLOWED_HOSTS = ['89.169.165.254', '127.0.0.1', 'localhost', 'backend', 'gioul.gotdns.ch']
 
 
 # Application definition
